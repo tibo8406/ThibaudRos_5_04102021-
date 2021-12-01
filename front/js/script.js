d40d710ebd43bfd61070d8db4bf79ee6243075c3
Thibaud Ros <thibaud.ros@gmail.com>
2021-12-01 09:04:15 +0100

    retrait de commentaire

--- front/js/script.js
+++ front/js/script.js
@@ -10,16 +10,10 @@ loadConfig().then(data => {
     //requete et redcuperation de produits dans l'api
     fetch(config.host + "/api/products").then(data => data.json())
         .then(jsonListProducts => {
-            /*class Product {
-                constructor(jsonProduct) {
-                    jsonProduct && Object.assign(this, jsonProduct);
-                }
-            }*/
             let productsHTML = "";
             //integration html des elements pour chaque produits
             for (let jsonProduct of jsonListProducts) {
                 const kanap = new Product(jsonProduct);
-                console.log(kanap);
                 productsHTML += `<a href="./product.html?id=${kanap._id}">
                 <article>
                   <img src="${kanap.imageUrl}" alt="${kanap.altTxt}, ${kanap.name}">
